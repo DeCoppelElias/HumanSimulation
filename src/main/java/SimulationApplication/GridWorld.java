627b1f290ab395e7e93d3efd6ad3ffe792b1a61b
EliasDeCoppel <elias.decoppel@gmail.com>
2026-08-30 19:07:13 +0200

    refactor: let GridWorld own a single RandomGenerator

--- src/main/java/SimulationApplication/GridWorld.java
+++ src/main/java/SimulationApplication/GridWorld.java
@@ -7,10 +7,10 @@ import SimulationApplication.GridContent.Entity.Human.HumanParameters;
 import SimulationApplication.GridContent.Food;
 import SimulationApplication.GridContent.FoodManager;
 import SimulationApplication.GridContent.GridContent;
-
 import java.util.ArrayList;
 import java.util.Hashtable;
 import java.util.Random;
+import java.util.random.RandomGenerator;
 
 public class GridWorld {
     private HumanParameters humanParameters = new HumanParameters();
@@ -31,16 +31,21 @@ public class GridWorld {
     private HumanManager humanManager;
     private FoodManager foodManager;
 
-    private Random random  = new Random();
+    private final RandomGenerator random;
 
-    public GridWorld(int width, int height){
+    public GridWorld(int width, int height) {
+        this(width, height, new Random());
+    }
+
+    public GridWorld(int width, int height, RandomGenerator random) {
+        this.random = random;
         this.width = width;
         this.height = height;
 
         gridWorld = new GridTile[height][width];
-        for(int x = 0; x < width; x++){
-            for(int y = 0; y < height; y++){
-                gridWorld[height - 1 - y][x] = new GridTile(x,y);
+        for (int x = 0; x < width; x++) {
+            for (int y = 0; y < height; y++) {
+                gridWorld[height - 1 - y][x] = new GridTile(x, y);
             }
         }
 
@@ -57,49 +62,54 @@ public class GridWorld {
         return height;
     }
 
-    public ArrayList<Integer> getEntityIds(GridPosition gridPosition){
+    public RandomGenerator getRandom() {
+        return this.random;
+    }
+
+    public ArrayList<Integer> getEntityIds(GridPosition gridPosition) {
         return getGridTile(gridPosition).getGridContentIds();
     }
 
-    public Boolean isHuman(int gridContentId){
+    public Boolean isHuman(int gridContentId) {
         return ids.get(gridContentId) instanceof Human;
     }
 
-    public Boolean checkDisplayability(int gridContentId){
-        if(!ids.containsKey(gridContentId)) return false;
+    public Boolean checkDisplayability(int gridContentId) {
+        if (!ids.containsKey(gridContentId)) return false;
         return ids.get(gridContentId).checkDisplayability();
     }
 
     public int getRange(int gridContentId) throws Exception {
-        if(!(ids.get(gridContentId) instanceof Human)) throw new Exception("GridContent linked to id doesn't have a range");
-        return ((Human)ids.get(gridContentId)).getViewRange();
+        if (!(ids.get(gridContentId) instanceof Human))
+            throw new Exception("GridContent linked to id doesn't have a range");
+        return ((Human) ids.get(gridContentId)).getViewRange();
     }
 
-    public GridPosition getGridPosition(int gridContentId){
+    public GridPosition getGridPosition(int gridContentId) {
         return ids.get(gridContentId).getGridPosition();
     }
 
-    public String getInfoString(int id){
+    public String getInfoString(int id) {
         GridContent content = ids.get(id);
         return content.toString();
     }
 
-    public Hashtable<String, Float> getInfo(int id){
+    public Hashtable<String, Float> getInfo(int id) {
         GridContent content = ids.get(id);
-        if(content == null) return new Hashtable<>();
+        if (content == null) return new Hashtable<>();
         return content.getInfo();
     }
 
-    public DataAnalytics getDataAnalytics(){
+    public DataAnalytics getDataAnalytics() {
         return this.dataAnalytics;
     }
 
-    public Hashtable<GridPosition, String> getInfo(){
+    public Hashtable<GridPosition, String> getInfo() {
         Hashtable<GridPosition, String> result = new Hashtable<GridPosition, String>();
 
-        for(int x = 0; x < width; x++){
-            for(int y = 0; y < height; y++){
-                GridPosition gridPosition = new GridPosition(x,y);
+        for (int x = 0; x < width; x++) {
+            for (int y = 0; y < height; y++) {
+                GridPosition gridPosition = new GridPosition(x, y);
                 String s = getGridTile(gridPosition).toString();
 
                 result.put(gridPosition, s);
@@ -109,41 +119,41 @@ public class GridWorld {
         return result;
     }
 
-    public int getDay(){
+    public int getDay() {
         return this.day;
     }
 
     public void addContent(GridContent content) {
-        if(!isWithinBounds(content.getGridPosition())) return;
+        if (!isWithinBounds(content.getGridPosition())) return;
 
         int id = idCounter;
         ids.put(id, content);
-        gridContents.put(content,id);
+        gridContents.put(content, id);
         idCounter++;
 
         GridTile gridTile = getGridTile(content.getGridPosition());
         gridTile.addContent(content, id);
 
-        if(content instanceof Human h) humanManager.addHuman(h);
-        if(content instanceof Food f) foodManager.addFood(f);
+        if (content instanceof Human h) humanManager.addHuman(h);
+        if (content instanceof Food f) foodManager.addFood(f);
     }
 
     public void removeContent(GridContent content) throws Exception {
-        if(!isWithinBounds(content.getGridPosition())) throw new Exception("Content is out of bounds");
+        if (!isWithinBounds(content.getGridPosition())) throw new Exception("Content is out of bounds");
         GridTile gridTile = getGridTile(content.getGridPosition());
 
         int id = gridContents.get(content);
         ids.remove(id);
         gridContents.remove(content);
 
-        if(!gridTile.contains(id)) throw new Exception("Content doesn't exist in gridworld");
+        if (!gridTile.contains(id)) throw new Exception("Content doesn't exist in gridworld");
 
         gridTile.removeContent(id);
 
-        if(content instanceof Human h){
+        if (content instanceof Human h) {
             humanManager.removeHuman(h);
         }
-        if(content instanceof Food f) foodManager.removeFood(f);
+        if (content instanceof Food f) foodManager.removeFood(f);
 
         content.setActive(false);
     }
@@ -162,8 +172,8 @@ public class GridWorld {
         dataAnalytics.updateData(day);
     }
 
-    public void refreshContent(GridContent content, GridPosition oldGridPosition){
-        if(content.getGridPosition().equals(oldGridPosition)) return;
+    public void refreshContent(GridContent content, GridPosition oldGridPosition) {
+        if (content.getGridPosition().equals(oldGridPosition)) return;
 
         GridTile oldGridTile = getGridTile(oldGridPosition);
         oldGridTile.removeContent(gridContents.get(content));
@@ -172,43 +182,46 @@ public class GridWorld {
         newGridTile.addContent(content, gridContents.get(content));
     }
 
-    public Boolean isWithinBounds(GridPosition gridPosition){
-        return gridPosition.getX() >= 0 && gridPosition.getY() >= 0 && gridPosition.getX() < width && gridPosition.getY() < height;
+    public Boolean isWithinBounds(GridPosition gridPosition) {
+        return gridPosition.getX() >= 0
+                && gridPosition.getY() >= 0
+                && gridPosition.getX() < width
+                && gridPosition.getY() < height;
     }
 
-    public Boolean containsFood(GridPosition gridPosition){
-        if(!isWithinBounds(gridPosition)) return false;
+    public Boolean containsFood(GridPosition gridPosition) {
+        if (!isWithinBounds(gridPosition)) return false;
         GridTile gridTile = getGridTile(gridPosition);
         return gridTile.containsFood();
     }
 
     public void collectFood(GridPosition gridPosition, Human human) {
-        if(!containsFood(gridPosition)) return;
+        if (!containsFood(gridPosition)) return;
         GridTile gridTile = getGridTile(gridPosition);
         gridTile.collectFood(human);
     }
 
-    public GridTile getGridTile(GridPosition gridPosition){
+    public GridTile getGridTile(GridPosition gridPosition) {
         return this.gridWorld[height - 1 - gridPosition.getY()][gridPosition.getX()];
     }
 
-    public void resetStatistics(){
+    public void resetStatistics() {
         this.dataAnalytics.resetStatistics();
     }
 
-    public ArrayList<GridPosition> getAllFoodPositions(){
+    public ArrayList<GridPosition> getAllFoodPositions() {
         return foodManager.getAllFoodPositions();
     }
 
-    public ArrayList<GridPosition> getAllHumanPositions(){
+    public ArrayList<GridPosition> getAllHumanPositions() {
         return humanManager.getAllHumanPositions();
     }
 
-    public ArrayList<Integer> getAllHumanIds(){
+    public ArrayList<Integer> getAllHumanIds() {
         return this.humanManager.getAllHumanIds();
     }
 
-    public Integer getId(GridContent gridContent){
+    public Integer getId(GridContent gridContent) {
         return this.gridContents.get(gridContent);
     }
 
@@ -228,55 +241,55 @@ public class GridWorld {
         this.humanParameters.setBreedCost(breedCost);
     }
 
-    public Hashtable<String, Integer> getHumanParameterInfo(){
+    public Hashtable<String, Integer> getHumanParameterInfo() {
         return humanParameters.getHumanParameterInfo();
     }
 
-    public void applyParameters(Hashtable<String, Integer> parameters){
+    public void applyParameters(Hashtable<String, Integer> parameters) {
         humanParameters.applyParameters(parameters);
     }
 
-    public void spawnHumanRandom(int amount){
-        for(int i = 0; i < amount; i++){
+    public void spawnHumanRandom(int amount) {
+        for (int i = 0; i < amount; i++) {
             GridPosition gridPosition = getRandomPosition();
 
             Human human = new Human(this, gridPosition, humanParameters);
         }
     }
 
-    public void spawnHumanPosition(int amount, GridPosition gridPosition){
-        for(int i = 0; i < amount; i++){
+    public void spawnHumanPosition(int amount, GridPosition gridPosition) {
+        for (int i = 0; i < amount; i++) {
             Human human = new Human(this, gridPosition, humanParameters);
         }
     }
 
-    public void spawnFoodRandom(int amount){
-        for(int i = 0; i < amount; i++){
+    public void spawnFoodRandom(int amount) {
+        for (int i = 0; i < amount; i++) {
             GridPosition gridPosition = getRandomPosition();
 
             Food food = new Food(this, gridPosition);
         }
     }
 
-    public void spawnFoodPosition(int amount, GridPosition gridPosition){
-        for(int i = 0; i < amount; i++){
+    public void spawnFoodPosition(int amount, GridPosition gridPosition) {
+        for (int i = 0; i < amount; i++) {
             Food food = new Food(this, gridPosition);
         }
     }
 
-    public GridPosition getRandomPosition(){
+    public GridPosition getRandomPosition() {
         int randomX = random.nextInt(0, getWidth());
         int randomY = random.nextInt(0, getHeight());
-        GridPosition gridPosition = new GridPosition(randomX,randomY);
+        GridPosition gridPosition = new GridPosition(randomX, randomY);
         return gridPosition;
     }
 
     @Override
     public String toString() {
         String result = "";
-        for(int y = 0; y < height; y++){
+        for (int y = 0; y < height; y++) {
             String row = "";
-            for(int x = 0; x < width; x++){
+            for (int x = 0; x < width; x++) {
                 row += this.gridWorld[y][x].toString() + " ";
             }
             row += "\n";
